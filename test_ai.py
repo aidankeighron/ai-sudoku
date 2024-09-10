75df536258dcf966a86e73788f5464a7402d1145
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-09-10 14:47:38 -0400

    action and tests

--- test_ai.py
+++ test_ai.py
@@ -1,38 +1,40 @@
-import numpy as np
+import numpy as np, pytest
 from ai import Perceptron, test_perceptron
 
-def test_or_dataset():
-    # OR dataset
-    x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.array([[0], [1], [1], [1]])
+class TestPerceptron:
+    def test_or_dataset(self):
+        # OR dataset
+        x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        y = np.array([[0], [1], [1], [1]])
 
-    p = Perceptron(x.shape[1], alpha=0.1)
-    p.fit(x, y, epochs=20)
+        p = Perceptron(x.shape[1], alpha=0.1)
+        p.fit(x, y, epochs=20)
 
-    for (value, target) in zip(x, y):
-        pred = p.predict(value)
-        assert target[0] == pred
+        for (value, target) in zip(x, y):
+            pred = p.predict(value)
+            assert target[0] == pred
 
-def test_and_dataset():
-    # OR dataset
-    x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.array([[0], [0], [0], [1]])
+    def test_and_dataset(self):
+        # OR dataset
+        x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        y = np.array([[0], [0], [0], [1]])
 
-    p = Perceptron(x.shape[1], alpha=0.1)
-    p.fit(x, y, epochs=20)
+        p = Perceptron(x.shape[1], alpha=0.1)
+        p.fit(x, y, epochs=20)
 
-    for (value, target) in zip(x, y):
-        pred = p.predict(value)
-        assert target[0] == pred
+        for (value, target) in zip(x, y):
+            pred = p.predict(value)
+            assert target[0] == pred
 
-def test_xor_dataset():
-    # OR dataset
-    x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.array([[0], [1], [1], [0]])
+    def test_xor_dataset(self):
+        # XOR dataset
+        x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        y = np.array([[0], [1], [1], [0]])
 
-    p = Perceptron(x.shape[1], alpha=0.1)
-    p.fit(x, y, epochs=20)
+        p = Perceptron(x.shape[1], alpha=0.1)
+        p.fit(x, y, epochs=20)
 
-    for (value, target) in zip(x, y):
-        pred = p.predict(value)
-        assert target[0] == pred
+        with pytest.raises(Exception):
+            for (value, target) in zip(x, y):
+                pred = p.predict(value)
+                assert target[0] == pred
