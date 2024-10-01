a1d2a2ec8d080fb745e45228e5eb631ad4bb3401
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-10-01 14:00:54 -0400

    keras nn

--- nn.py
+++ nn.py
@@ -1,4 +1,11 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+from sklearn.metrics import classification_report
 from sklearn.metrics import mean_squared_error
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Dense, Input
+import matplotlib.pyplot as plt
 from numpy import ndarray
 from typing import List
 from board import Board
@@ -57,10 +64,10 @@ def test_perceptron():
 # test_perceptron()
 
 class NeuralNetwork:
-    def __init__(self, layers, alpha=0.1):
+    def __init__(self, layers, learning_rate=0.1):
         self.weights = []
         self.layers = layers
-        self.alpha = alpha
+        self.learning_rate = learning_rate
 
         for i in np.arange(0, len(layers)-2):
             w = np.random.randn(layers[i]+1, layers[i+1]+1)
@@ -88,7 +95,7 @@ class NeuralNetwork:
             if epoch == 0 or (epoch+1) % display_update == 0:
                 loss = self.calculate_loss(x, y)
                 print(f"[INFO] epoch={epoch+1}, loss={round(loss, 5)}")
-    
+
     def fit_partial(self, x, y):
         a = [np.atleast_2d(x)]
 
@@ -109,7 +116,7 @@ class NeuralNetwork:
         d = d[::-1]
 
         for layer in np.arange(0, len(self.weights)):
-            self.weights[layer] += -self.alpha * a[layer].T.dot(d[layer])
+            self.weights[layer] += -self.learning_rate * a[layer].T.dot(d[layer])
 
     def predict(self, x, add_bias=True):
         p = np.atleast_2d(x)
@@ -130,7 +137,7 @@ class NeuralNetwork:
         return loss 
 
 def test_nn():
-    nn = NeuralNetwork([2, 2, 1], alpha=0.5)
+    nn = NeuralNetwork([2, 2, 1], learning_rate=0.5)
     x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([[0], [1], [1], [0]])
 
@@ -143,7 +150,7 @@ def test_nn():
         print(f"[INFO] data={value}, ground-truth={target[0]}, pred={round(pred, 4)}, step={step}")
 
 def run_nn():
-    nn = NeuralNetwork([81, 81, 81, 81, 81], alpha=0.5)
+    nn = NeuralNetwork([81, 160, 120, 100, 81], learning_rate=0.5)
 
     start_seed = 0
     end_seed = 1000
@@ -157,7 +164,6 @@ def run_nn():
     with open("data.json", "w") as f:
         json.dump({"data": x.tolist(), "answer": y.tolist(), "start_seed": start_seed, "end_seed": end_seed}, f)
 
-    # TODO generator for y
     print("Training")
     nn.fit(x, y, epochs=5000)
     print("Training Done")
@@ -174,4 +180,59 @@ def run_nn():
         print(f"[INFO], AI mse={np.round(ai_mse, 2)}, Nothing mse={np.round(nothing_mse, 2)}")
 
 # test_nn()
-run_nn()
+# run_nn()
+
+class Keras:
+    def __init__(self, learning_rate=0.01):
+        self.model = Sequential()
+        # inputs = Input(shape=(81,))
+        self.model.add(Dense(256, input_shape=(81,), activation="sigmoid"))
+        # self.model.add(Dense(256, activation="sigmoid")(inputs))
+        self.model.add(Dense(128, activation="sigmoid"))
+        self.model.add(Dense(81, activation="softmax"))
+        self.learning_rate = learning_rate
+
+    def fit(self, train_x, train_y, test_x, test_y, epochs=5000):
+
+        sgd = SGD(self.learning_rate)
+        self.model.compile(loss="categorical_crossentropy", optimizer=sgd,
+            metrics=["accuracy"])
+        H = self.model.fit(train_x, train_y, validation_data=(test_x, test_y),
+            epochs=epochs, batch_size=128)
+        
+        print("[INFO] evaluating network...")
+        predictions = self.model.predict(test_x, batch_size=128)
+        print(classification_report(test_y.argmax(axis=1), predictions.argmax(axis=1)))
+        
+        plt.style.use("ggplot")
+        plt.figure()
+        plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+        plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+        plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+        plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
+        plt.title("Training Loss and Accuracy")
+        plt.xlabel("Epoch #")
+        plt.ylabel("Loss/Accuracy")
+        plt.legend()
+        plt.savefig("plot.png")
+        
+def test_keras():
+    keras = Keras(learning_rate=0.01)
+    
+    start_seed = 0
+    end_seed = 1000
+
+    training_data: List[Board] = [Board(difficulty=0.2, seed=i) for i in range(start_seed, end_seed)]
+    print("Boards Generated")
+    train_y = np.array(np.array([data.solve().get_board_1d() for data in training_data])/10, dtype=np.float64)
+    print("Answer Generated")
+    train_x = np.array(np.array([data.get_board_1d() for data in training_data])/10, dtype=np.float64)
+    print("Data Generated")
+
+    testing_data: List[Board] = [Board(difficulty=0.2, seed=i) for i in range(end_seed, end_seed+5)]
+    test_y = np.array(np.array([data.solve().get_board_1d() for data in testing_data])/10, dtype=np.float64)
+    test_x = np.array(np.array([data.get_board_1d() for data in testing_data])/10, dtype=np.float64)
+
+    keras.fit(train_x, train_y, test_x, test_y, epochs=1000)
+
+test_keras()
